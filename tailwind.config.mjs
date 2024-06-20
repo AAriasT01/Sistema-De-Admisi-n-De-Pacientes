23007893b44aef9f64bee755c5c4a82f7d86846a
AAriasT <angieariastorres2000@gmailcom>
2024-06-20 00:45:07 -0600

    página de inicio completa

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+		extend: {
+			backgroundImage: {
+			  'hero-pattern': "url('src/img/banner.jpeg')",
+			  'separador1-pattern': "url('src/img/separador1.jpeg')",
+			},
+		  },
+		},
+		plugins: [],
+	  };
