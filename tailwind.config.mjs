8b7af3374251f76c4940214d6f0bd674e0beaeb8
AAriasT <angieariastorres2000@gmailcom>
2024-06-20 19:08:55 -0600

    0.1.0 Todas las páginas completas

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+
+module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			backgroundImage: {
-			  'hero-pattern': "url('src/img/banner.jpeg')",
-			  'separador1-pattern': "url('src/img/separador1.jpeg')",
-			},
-		  },
+	  extend: {
+		screens: {
+		  'xs': {'max': '639px'},
 		},
-		plugins: [],
-	  };
+		backgroundImage: {
+		  'hero-pattern': "url('src/img/banner.jpeg')",
+		  'separador1-pattern': "url('src/img/separador1.jpeg')",
+		  'back-pattern': "url('src/img/clinica.jpg')",
+		},
+	  },
+	},
+	plugins: [
+		
+	],
+  };
