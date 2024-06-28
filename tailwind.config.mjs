f78bc335e833afa38077e6450050d96fa4078200
AAriasT <angieariastorres2000@gmailcom>
2024-06-27 20:41:08 -0600

    0.1.4 cambio en rutas

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,9 +8,9 @@ module.exports = {
 		  'xs': {'max': '639px'},
 		},
 		backgroundImage: {
-		  'hero-pattern': "url('public/img/banner.jpeg')",
-		  'separador1-pattern': "url('public/img/separador1.jpeg')",
-		  'back-pattern': "url('public/img/clinica.jpg')",
+		  'hero-pattern': "url('/img/banner.jpeg')",
+		  'separador1-pattern': "url('/img/separador1.jpeg')",
+		  'back-pattern': "url('/img/clinica.jpg')",
 		},
 	  },
 	},
